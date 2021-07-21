62fa5a9c6f552a70b1877b1084eea907fd55aea8
Bakhtiar Abdullah <backids99@gmail.com>
2021-07-21 11:48:00 +0700

    [adding] progress configuration enabled and drawable

--- echart/src/main/java/id/bafika/echart/ECharts.java
+++ echart/src/main/java/id/bafika/echart/ECharts.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.webkit.WebSettings;
@@ -25,6 +26,9 @@ public class ECharts extends RelativeLayout {
 
     private String theme;
     private GsonOption options;
+
+    private boolean progressEnabled = true;
+    private Drawable progressDrawable;
 
     public ECharts(Context context) {
         super(context);
@@ -60,6 +64,9 @@ public class ECharts extends RelativeLayout {
 
     @SuppressLint("SetJavaScriptEnabled")
     public void build() {
+        progressBar.setEnabled(progressEnabled);
+        progressBar.setProgressDrawable(progressDrawable);
+
         String content  = Utils.getMain(getContext());
         content         = content.replace("%theme%", theme);
         content         = content.replace("%options%", options.toString());
@@ -94,5 +101,13 @@ public class ECharts extends RelativeLayout {
 
     public void setOptions(GsonOption options) {
         this.options = options;
+    }
+
+    public void setProgressEnabled(boolean progressEnabled) {
+        this.progressEnabled = progressEnabled;
+    }
+
+    public void setProgressDrawable(Drawable progressDrawable) {
+        this.progressDrawable = progressDrawable;
     }
 }
